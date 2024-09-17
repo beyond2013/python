489b8a32631e527e309041c7438186dce3fbd594
beyond2013 <imran.cs.uob@gmail.com>
2024-09-17 17:58:24 +0500

    add code to scrape system requirements

--- webscrapper/scrape.py
+++ webscrapper/scrape.py
@@ -11,7 +11,7 @@ def scrape_content(url):
     Returns:
         The extracted content as a string.
     """
-
+    html_content=""
     print("Fetching URL...")
     response = requests.get(url)
     if response.status_code != 200:
@@ -22,24 +22,36 @@ def scrape_content(url):
     soup = BeautifulSoup(response.content, 'html.parser')
 
     print("Finding div with id 'appHubAppName'...")
-    content_div = soup.find('div', id='appHubAppName')
+    game_name= soup.find('div', id='appHubAppName')
 
-    if content_div is None:
+    if game_name is None:
         print("Error: Could not find the title of the Game")
-    html_content = "<title>" + content_div.text.strip() + "</title>"
+    html_content = "<title>" + game_name.text.strip() + "</title>"
     #print(html_content)
-    print("Finding div with id 'game_area_description'...")
-    content_div = soup.find('div', id='game_area_description')
+    # print("Finding div with id 'game_area_description'...")
+    game_area_description = soup.find('div', id='game_area_description')
    
-    if content_div is None:
+    if game_area_description is None:
         print("Error: Could not find div with id 'game_area_description'")
         return None
 
-    print("Extracting content...")
-    inner_html = "".join(str(child) for child in content_div.contents)
+    #print("Extracting content...")
+    inner_html = "".join(str(child) for child in game_area_description.contents)
     pretty_html = BeautifulSoup(inner_html, 'html.parser').prettify()
     html_content += pretty_html 
+
+    sys_req = soup.find('div', class_="game_page_autocollapse sys_req")
+
+    if sys_req is None:
+        print("Error: Could not find div with class 'game_page_autocollapse sys_req'")
+        return None
+
+    inner_html = "".join(str(child) for child in sys_req.contents)
+    pretty_html = BeautifulSoup(inner_html, 'html.parser').prettify()
+    html_content += pretty_html 
+
     markdown_content = markdown.markdown(html_content)
+
 
     return markdown_content
 
